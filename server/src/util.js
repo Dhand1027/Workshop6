6dc671ea6939cb80848598f45bc5f62d27dfca52
Dylan Hand <dhand@umass.edu>
2017-04-13 18:05:34 -0400

    (Fixing util.js so that reverseString works properly, even though we  are to skip the first part of the assignment)

--- server/src/util.js
+++ server/src/util.js
@@ -1,7 +1,7 @@
 function reverseString(str) {
   var reversed = "";
-  for (var i = 0; i < str.length; i++) {
-    reversed += str[str.length - i];
+  for (var i = 0; i < str.length ; i++) {
+    reversed += str[str.length - i - 1];
   }
   return reversed;
 }
