55af400c21a523b03d65c55c39a56fec4f89d7b3
Dylan Hand <dhand@umass.edu>
2017-04-13 19:37:34 -0400

    validation

--- server/src/server.js
+++ server/src/server.js
@@ -1,22 +1,94 @@
-//var util = require('./util.js');
-var readDocument = require('./database').readDocument;
-
-
 // Imports the express Node module.
 var express = require('express');
 // Creates an Express server.
 var app = express();
 
-// You run the server from `server`, so `../client/build` is `server/../client/build`.
-// '..' means "go up one directory", so this translates into `client/build`!
-app.use(express.static('../client/build'));
-
+// Import the bodyParse for the middleware
 var bodyParser = require('body-parser');
 app.use(bodyParser.text());
 
-// Starts the server on port 3000!
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+// Import the readDocument function from database.js
+var database = require('./database');
+var readDocument = database.readDocument;
+
+var StatusUpdateSchema = require('./schemas/statusupdate.json');
+var validate = require('express-jsonschema').validate;
+var writeDocument = database.writeDocument;
+var addDocument = database.addDocument;
+
+
+
+// You run the server from `server`, so `../client/build` is `server/../client/build`.
+// '..' means "go up one directory", so this translates into `client/build`!
+app.use(express.static('../client/build'));
+// Support receiving text in HTTP request bodies
+app.use(bodyParser.text());
+// Support receiving JSON in HTTP request bodies
+app.use(bodyParser.json());
+
+/**
+ * Adds a new status update to the database.
+ */
+function postStatusUpdate(user, location, contents) {
+  // If we were implementing this for real on an actual server, we would check
+  // that the user ID is correct & matches the authenticated user. But since
+  // we're mocking it, we can be less strict.
+
+  // Get the current UNIX time.
+  var time = new Date().getTime();
+  // The new status update. The database will assign the ID for us.
+  var newStatusUpdate = {
+    "likeCounter": [],
+    "type": "statusUpdate",
+    "contents": {
+      "author": user,
+      "postDate": time,
+      "location": location,
+      "contents": contents,
+      "likeCounter": []
+    },
+    // List of comments on the post
+    "comments": []
+  };
+
+  // Add the status update to the database.
+  // Returns the status update w/ an ID assigned.
+  newStatusUpdate = addDocument('feedItems', newStatusUpdate);
+
+  // Add the status update reference to the front of the current user's feed.
+  var userData = readDocument('users', user);
+  var feedData = readDocument('feeds', userData.feed);
+  feedData.contents.unshift(newStatusUpdate._id);
+
+  // Update the feed object.
+  writeDocument('feeds', feedData);
+
+  // Return the newly-posted object.
+  return newStatusUpdate;
+}
+
+// `POST /feeditem { userId: user, location: location, contents: contents  }`
+app.post('/feeditem',
+         validate({ body: StatusUpdateSchema }), function(req, res) {
+  // If this function runs, `req.body` passed JSON validation!
+  var body = req.body;
+  var fromUser = getUserIdFromToken(req.get('Authorization'));
+
+  // Check if requester is authorized to post this status update.
+  // (The requester must be the author of the update.)
+  if (fromUser === body.userId) {
+    var newUpdate = postStatusUpdate(body.userId, body.location, body.contents);
+    // When POST creates a new resource, we should tell the client about it
+    // in the 'Location' header and use status code 201.
+    res.status(201);
+    res.set('Location', '/feeditem/' + newUpdate._id);
+     // Send the update!
+    res.send(newUpdate);
+  } else {
+    // 401: Unauthorized.
+    res.status(401).end();
+  }
 });
 
 /**
@@ -25,13 +97,10 @@ app.listen(3000, function () {
 function getFeedItemSync(feedItemId) {
   var feedItem = readDocument('feedItems', feedItemId);
   // Resolve 'like' counter.
-  feedItem.likeCounter = feedItem.likeCounter.map((id) =>
-                            readDocument('users', id));
-  // Assuming a StatusUpdate. If we had other types of
-  // FeedItems in the DB, we would
+  feedItem.likeCounter = feedItem.likeCounter.map((id) => readDocument('users', id));
+  // Assuming a StatusUpdate. If we had other types of FeedItems in the DB, we would
   // need to check the type and have logic for each type.
-  feedItem.contents.author = readDocument('users',
-	feedItem.contents.author);
+  feedItem.contents.author = readDocument('users', feedItem.contents.author);
   // Resolve comment author.
   feedItem.comments.forEach((comment) => {
     comment.author = readDocument('users', comment.author);
@@ -40,35 +109,18 @@ function getFeedItemSync(feedItemId) {
 }
 
 /**
- * Get the feed data for a particular user.
+ * Emulates a REST call to get the feed data for a particular user.
  */
 function getFeedData(user) {
   var userData = readDocument('users', user);
   var feedData = readDocument('feeds', userData.feed);
-  // While map takes a callback, it is synchronous,
-  // not asynchronous. It calls the callback immediately.
+  // While map takes a callback, it is synchronous, not asynchronous.
+  // It calls the callback immediately.
   feedData.contents = feedData.contents.map(getFeedItemSync);
   // Return FeedData with resolved references.
   return feedData;
 }
-
-/**
- * Get the feed data for a particular user.
- */
-app.get('/user/:userid/feed', function(req, res) {
-  var userid = req.params.userid;
-  var fromUser = getUserIdFromToken(req.get('Authorization'));
-  // userid is a string. We need it to be a number.
-  // Parameters are always strings.
-  var useridNumber = parseInt(userid, 10);
-  if (fromUser === useridNumber) {
-    // Send response.
-    res.send(getFeedData(userid));
-  } else {
-    // 401: Unauthorized request.
-    res.status(401).end();
-  }
-});
+module.exports.getFeedData = getFeedData;
 
 /**
  * Get the user ID from a token. Returns -1 (an invalid ID)
@@ -95,3 +147,38 @@ function getUserIdFromToken(authorizationLine) {
     return -1;
   }
 }
+
+/**
+ * Get the feed data for a particular user.
+ */
+app.get('/user/:userid/feed', function(req, res) {
+  var userid = req.params.userid;
+  var fromUser = getUserIdFromToken(req.get('Authorization'));
+  // userid is a string. We need it to be a number.
+  // Parameters are always strings.
+  var useridNumber = parseInt(userid, 10);
+  if (fromUser === useridNumber) {
+    // Send response.
+    res.send(getFeedData(userid));
+  } else {
+    // 401: Unauthorized request.
+    res.status(401).end();
+  }
+});
+
+/**
+ * Translate JSON Schema Validation failures into error 400s.
+ */
+app.use(function(err, req, res, next) {
+  if (err.name === 'JsonSchemaValidation') {
+    // Set a bad request http response status
+    res.status(400).end();
+  } else {
+    // It's some other sort of error; pass it to next error middleware handler
+    next(err);
+  }
+});
+// Starts the server on port 3000!
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
